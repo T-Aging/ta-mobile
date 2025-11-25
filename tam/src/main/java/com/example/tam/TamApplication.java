047a772dba41bb19254a56eac554e0020f653e37
jeonggwwon <gyowon1026@gmail.com>
2025-11-25 20:23:29 +0900

    update

---
+++ tam/src/main/java/com/example/tam/TamApplication.java
@@ -0,0 +1,13 @@
+package com.example.tam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamApplication.class, args);
+	}
+
+}
