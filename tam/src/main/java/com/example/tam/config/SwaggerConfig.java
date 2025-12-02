a104e1f4c215f09e64369b9ab5cf1c7cf2fb391d
Sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2025-12-02 17:04:40 +0900

    Create SwaggerConfig.java

---
+++ tam/src/main/java/com/example/tam/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.tam.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TAM Backend API")
+                        .description("T-Aging Mobile 백엔드 API 문서")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("TAM Team")
+                                .email("support@tam.com")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", 
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요 (Bearer 제외)")));
+    }
+}
