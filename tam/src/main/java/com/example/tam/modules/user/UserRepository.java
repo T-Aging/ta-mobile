4bfe5e1d6f21a83620aaa6872538ecd37103efe1
Sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2025-12-03 13:55:24 +0900

    Update UserRepository.java

--- tam/src/main/java/com/example/tam/modules/user/UserRepository.java
+++ tam/src/main/java/com/example/tam/modules/user/UserRepository.java
@@ -9,16 +9,3 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 }
-
-// modules/auth/KakaoRepository.java
-package com.example.tam.modules.auth;
-
-import com.example.tam.common.entity.Kakao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.Optional;
-
-@Repository
-public interface KakaoRepository extends JpaRepository<Kakao, String> {
-    Optional<Kakao> findByUserId(Integer userId);
-}
