535575d3d86e747eec08006c98cf6c725c94caa8
Sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2025-12-03 13:35:06 +0900

    Create CustomHeaderRepository.java

---
+++ tam/src/main/java/com/example/tam/modules/custom/CustomHeaderRepository.java
@@ -0,0 +1,12 @@
+package com.example.tam.modules.custom;
+
+import com.example.tam.common.entity.CustomHeader;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CustomHeaderRepository extends JpaRepository<CustomHeader, Integer> {
+    List<CustomHeader> findByUserId(Integer userId);
+    List<CustomHeader> findByUserIdOrderByCustomIdDesc(Integer userId);
+}
