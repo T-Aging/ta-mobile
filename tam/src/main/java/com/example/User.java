72aa7c4e6165a97701745984b2fa436b24973467
Sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2025-12-03 13:32:50 +0900

    Create User.java

---
+++ tam/src/main/java/com/example/User.java
@@ -0,0 +1,44 @@
+package com.example.tam.common.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @CreatedDate
+    @Column(name = "signup_date", updatable = false)
+    private LocalDateTime signupDate;
+
+    @LastModifiedDate
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+
+    @Column(name = "user_qr")
+    private String userQr;
+}
