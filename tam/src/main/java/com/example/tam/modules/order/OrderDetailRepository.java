ad5b79e271898063229c4f5e5430ae2e6dab9f3c
Sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2025-12-03 13:36:03 +0900

    Create OrderDetailRepository.java

---
+++ tam/src/main/java/com/example/tam/modules/order/OrderDetailRepository.java
@@ -0,0 +1,11 @@
+package com.example.tam.modules.order;
+
+import com.example.tam.common.entity.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
+    List<OrderDetail> findByOrderId(Integer orderId);
+}
