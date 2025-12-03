1c1125adaf08bf89e2a08bec4005c416fb5f31ab
Sohyunxxi <104574382+sohyunxxi@users.noreply.github.com>
2025-12-03 13:37:43 +0900

    Create StoreRepository.java

---
+++ tam/src/main/java/com/example/tam/modules/store/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.example.tam.modules.store;
+
+import com.example.tam.common.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Integer> {
+}
